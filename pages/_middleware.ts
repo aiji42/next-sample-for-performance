fb2d88c22e6e170e1ea2fc49357187f7aeb2fbed
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-11-30 19:57:23 +0900

    remove middleware

--- pages/_middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server'
-
-export const middleware = (req: NextRequest) => {
-  if (req.nextUrl.pathname.match(/\.(ico|jpg|png)$/)) return
-  if (req.cookies['cacheable'] !== 'true') return
-
-  console.log('SSG mode', `/ssg${req.nextUrl.pathname}`)
-  return NextResponse.rewrite(`/ssg${req.nextUrl.pathname}`)
-}
